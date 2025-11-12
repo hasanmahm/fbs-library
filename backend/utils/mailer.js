b986133456b133c984aa8b7009923422d7de7fff
hasanmahm <alvimahmud55@gmail.com>
2025-11-12 15:40:11 +0600

    mailer package change

--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -4,6 +4,9 @@ dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
+  host: process.env.EMAIL_HOST, // smtp-relay.brevo.com
+  port: process.env.EMAIL_PORT, // 587
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
